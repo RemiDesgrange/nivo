9b730226c1bb40f331b066ddb2a1d2949bf8cf94
Rémi Desgrange <remi.desgrange@camptocamp.com>
2019-05-07 22:21:41 +0200

    setup cli

--- api/setup.py
+++ api/setup.py
@@ -3,12 +3,19 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     REQUIREMENTS = f.read().split('\n')
 
-setup(name='Distutils',
+setup(name='nivo_api',
       version='0.1',
-      description='APi to serve snow opendata from meteofrance',
+      description='API to serve snow opendata from meteofrance',
       author='Remi Desgrange',
       author_email='remi+nivo@desgran.ge',
       url='',
       packages=find_packages(),
-      install_requires=REQUIREMENTS
-    )
+      install_requires=REQUIREMENTS,
+      entry_points='''
+            [console_scripts]
+            import_last_nivo_data=nivo_api.cli:import_last_nivo_data
+            import_all_nivo_data=nivo_api.cli:import_all_nivo_data
+            import_last_bra=nivo_api.cli:import_last_bra
+            import_all_bra=nivo_api.cli:import_all_bra
+        ''',
+      )
