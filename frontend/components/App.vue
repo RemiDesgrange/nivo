1065e8ae778a8933fe1a1db888f22d79f80fc704
Rémi Desgrange <remi.desgrange@camptocamp.com>
2019-08-23 20:40:58 +0200

    start working on the fronend

---
+++ frontend/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid">
+    <!-- navbar-->
+    <div class="row">
+      <div class="col-6">BRA</div>
+      <div class="col-6">map</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
